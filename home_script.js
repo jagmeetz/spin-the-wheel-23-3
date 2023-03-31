0cae01cdf3322818c79cf80ca0759995084297a3
Jagmeet Singh <jagmeettakkar.3599@gmail.com>
2023-03-31 12:50:52 +0530

    spin wheel generation acc to no. of teachers

--- home_script.js
+++ home_script.js
@@ -5,6 +5,10 @@ const spinBtn = document.getElementById("spin-btn");
 const finalValue = document.getElementById("final-value");
 //Object that stores values of minimum and maximum angle for a value
 const rotationValues = [
+  { minDegree: 0, maxDegree: 360, value: 1 },
+];
+
+const rotationValues6 = [
   { minDegree: 0, maxDegree: 30, value: 2 },
   { minDegree: 31, maxDegree: 90, value: 1 },
   { minDegree: 91, maxDegree: 150, value: 6 },
@@ -13,8 +17,27 @@ const rotationValues = [
   { minDegree: 271, maxDegree: 330, value: 3 },
   { minDegree: 331, maxDegree: 360, value: 2 },
 ];
+const rotationValues4 = [
+  { minDegree: 0, maxDegree: 90, value: 1 },
+  { minDegree: 91, maxDegree: 180, value: 4 },
+  { minDegree: 181, maxDegree: 270, value: 3 },
+  { minDegree: 271, maxDegree: 360, value: 2 },
+];
+
+const rotationValues3 = [
+  { minDegree: 0, maxDegree: 90, value: 1 },
+  { minDegree: 91, maxDegree: 210, value: 3 },
+  { minDegree: 211, maxDegree: 330, value: 2 },
+  { minDegree: 331, maxDegree: 360, value: 1 },
+
+];
+const rotationValues2 = [
+  { minDegree: 0, maxDegree: 90, value: 1 },
+  { minDegree: 91, maxDegree: 270, value: 2 },
+  { minDegree: 271, maxDegree: 360, value: 1 },
+];
 //Size of each piece
-const data = [1, 1, 1, 1, 1, 1];
+const data = [1, 1,1,1,1,1];
 //background color for each piece
 var pieColors = [
   "#8b35bc",
@@ -25,85 +48,8 @@ var pieColors = [
   "#b163da",
 ];
 //Create chart
-let myChart = new Chart(wheel, {
-  //Plugin for displaying text on pie chart
-  plugins: [ChartDataLabels],
-  //Chart Type Pie
-  type: "pie",
-  data: {
-    //Labels(values which are to be displayed on chart)
-    labels: [1, 2, 3, 4, 5, 6],
-    //Settings for dataset/pie
-    datasets: [
-      {
-        backgroundColor: pieColors,
-        data: data,
-      },
-    ],
-  },
-  options: {
-    //Responsive chart
-    responsive: true,
-    animation: { duration: 0 },
-    plugins: {
-      //hide tooltip and legend
-      tooltip: false,
-      legend: {
-        display: false,
-      },
-      //display labels inside pie chart
-      datalabels: {
-        color: "#ffffff",
-        formatter: (_, context) => context.chart.data.labels[context.dataIndex],
-        font: { size: 24 },
-      },
-    },
-  },
-});
-//display value based on the randomAngle
-const valueGenerator = (angleValue) => {
-  for (let i of rotationValues) {
-    //if the angleValue is between min and max then display it
-    if (angleValue >= i.minDegree && angleValue <= i.maxDegree) {
-      finalValue.innerHTML = `<p>Value: ${i.value}</p>`;
-      spinBtn.disabled = false;
-      break;
-    }
-  }
-};
-//Spinner count
-let count = 0;
-//100 rotations for animation and last rotation for result
-let resultValue = 101;
-//Start spinning
-spinBtn.addEventListener("click", () => {
-  spinBtn.disabled = true;
-  //Empty final value
-  finalValue.innerHTML = `<p>Good Luck!</p>`;
-  //Generate random degrees to stop at
-  let randomDegree = Math.floor(Math.random() * 360);
-  //Interval for rotation animation
-  let rotationInterval = window.setInterval(() => {
-    //Set rotation for piechart
-    /*
-    Initially to make the piechart rotate faster we set resultValue to 101 so it rotates 101 degrees at a time and this reduces by 1 with every count. Eventually on last rotation we rotate by 1 degree at a time.
-    */
-    myChart.options.rotation = myChart.options.rotation + resultValue;
-    //Update chart with new value;
-    myChart.update();
-    //If rotation>360 reset it back to 0
-    if (myChart.options.rotation >= 360) {
-      count += 1;
-      resultValue -= 5;
-      myChart.options.rotation = 0;
-    } else if (count > 15 && myChart.options.rotation == randomDegree) {
-      valueGenerator(randomDegree);
-      clearInterval(rotationInterval);
-      count = 0;
-      resultValue = 101;
-    }
-  }, 10);
-});
+
+
 ////spin wheel//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 ////fetching academy name and retreiving teacher names,classes and other related data //////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -148,3 +94,104 @@ function render_table() {
 }
 render_table();
 ////render table using table_data //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+////render chart //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+let myChart;
+function render_chart(){
+  myChart = new Chart(wheel, {
+    //Plugin for displaying text on pie chart
+    plugins: [ChartDataLabels],
+    //Chart Type Pie
+    type: "pie",
+    data: {
+      //Labels(values which are to be displayed on chart)
+      labels: [1, 2, 3, 4, 5, 6],
+      //Settings for dataset/pie
+      datasets: [
+        {
+          backgroundColor: pieColors,
+          data: new Array(table_data.length).fill(1),
+        },
+      ],
+    },
+    options: {
+      //Responsive chart
+      responsive: true,
+      animation: { duration: 0 },
+      plugins: {
+        //hide tooltip and legend
+        tooltip: false,
+        legend: {
+          display: false,
+        },
+        //display labels inside pie chart
+        datalabels: {
+          color: "#ffffff",
+          formatter: (_, context) => context.chart.data.labels[context.dataIndex],
+          font: { size: 24 },
+        },
+      },
+    },
+  });
+}
+render_chart();
+////render chart //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let rotation_value_selector;
+if (table_data.length==2)
+{
+  rotation_value_selector= rotationValues2
+}
+else if (table_data.length==3){
+  rotation_value_selector=rotationValues3
+}
+else if (table_data.length==4){
+  rotation_value_selector=rotationValues4
+}
+else{
+  rotation_value_selector=rotationValues
+}
+//display value based on the randomAngle
+const valueGenerator = (angleValue) => {
+  for (let i of rotation_value_selector) {
+    //if the angleValue is between min and max then display it
+    if (angleValue >= i.minDegree && angleValue <= i.maxDegree) {
+      finalValue.innerHTML = `<p>Value: ${i.value}</p>`;
+      spinBtn.disabled = false;
+      break;
+    }
+  }
+};
+//Spinner count
+let count = 0;
+//100 rotations for animation and last rotation for result
+let resultValue = 101;
+//Start spinning
+spinBtn.addEventListener("click", () => {
+  spinBtn.disabled = true;
+  //Empty final value
+  finalValue.innerHTML = `<p>Good Luck!</p>`;
+  //Generate random degrees to stop at
+  let randomDegree = Math.floor(Math.random() * 360);
+  //Interval for rotation animation
+  let rotationInterval = window.setInterval(() => {
+    //Set rotation for piechart
+    /*
+    Initially to make the piechart rotate faster we set resultValue to 101 so it rotates 101 degrees at a time and this reduces by 1 with every count. Eventually on last rotation we rotate by 1 degree at a time.
+    */
+    myChart.options.rotation = myChart.options.rotation + resultValue;
+    //Update chart with new value;
+    myChart.update();
+    //If rotation>360 reset it back to 0
+    if (myChart.options.rotation >= 360) {
+      count += 1;
+      resultValue -= 5;
+      myChart.options.rotation = 0;
+    } else if (count > 15 && myChart.options.rotation == randomDegree) {
+      valueGenerator(randomDegree);
+      clearInterval(rotationInterval);
+      count = 0;
+      resultValue = 101;
+    }
+  }, 10);
+});
